caf3bc481ad0164ce0e966a81a8f7ac6d1067591
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-03-10 00:10:56 +0530

    Uploadthing setup

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/anyone-can-visit-this-route"],
+  // publicRoutes: ["/anyone-can-visit-this-route"],
   // Routes that can always be accessed, and have
   // no authentication information
+  publicRoutes: ["/api/uploadthing"],
   ignoredRoutes: ["/no-auth-in-this-route"],
 });
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+    // Your Next.js configuration options here
+    images: {
+        domains: ['utfs.io'],
+    },
+};
+// export default nextConfig;
